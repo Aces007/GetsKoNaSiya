2c43fd20f2f4479c97b308b95b5932c4747cb401
Istong Lang Malakas <clavanoace@gmail.com>
2021-11-02 19:14:40 -0700

    revised my program a little

--- Money=nApples.py
+++ Money=nApples.py
@@ -9,12 +9,12 @@ def GaanoKamahalBaAngMansanas():
 
 
 def MansanasNaKayaMongMabili():
-    ApplesNaMabibiliMo = int(TotalNgPeraNaKayaMoGastusin) // (TotalNgMansanasNaGustoMo)
+    ApplesNaMabibiliMo = int(MagkanoHawakMoNgayon) // (MagkanoBaPresyoNgMansanas)
     return ApplesNaMabibiliMo
 
 
 def ItoLahatNgSukliMo():
-    MagkanoMatitiraSaPeraMo = int(TotalNgPeraNaKayaMoGastusin) % (TotalNgMansanasNaGustoMo)
+    MagkanoMatitiraSaPeraMo = int(MagkanoHawakMoNgayon) % (MagkanoBaPresyoNgMansanas)
     return MagkanoMatitiraSaPeraMo
 
 
@@ -22,12 +22,18 @@ def MagkanoLahatNgNagastosKo():
     print(f"You can buy {MabibiliMongApple} apples and your change is {SukliMoAy}")
 
 
-TotalNgPeraNaKayaMoGastusin = MagkanoPeraNaHawakMoNgayon()
-TotalNgMansanasNaGustoMo = GaanoKamahalBaAngMansanas()
+# 1stStep: These lines will ask for the money you possess right now and the price of the apple you want to purchase.
+MagkanoHawakMoNgayon = MagkanoPeraNaHawakMoNgayon()
+MagkanoBaPresyoNgMansanas = GaanoKamahalBaAngMansanas()
 
-
+# 2ndStep: Then this will perform the math, and calculate the number of apples that you can buy with your money 
+# and consequently it will show the amount of change you will receive from your purchase.
 MabibiliMongApple = MansanasNaKayaMongMabili()
 SukliMoAy = ItoLahatNgSukliMo()
 
-
+# Lastly, this will present the total of your expenditures all in all.
 MagkanoLahatNgNagastosKo()
+
+
+# I did not insert any parameters in this program since it was not necessary.
+# Throughout the process of learning the fundamentals, I learned that 2 spaces between each line is a tradition.
